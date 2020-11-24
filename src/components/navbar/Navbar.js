5cd4ea627f0e298a18d3e94b56395234ca0106e3
Rob Romaniw <robert.romaniw@argogroupus.com>
2020-11-23 21:28:57 -0500

    navbar and styled-components

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Nav, NavbarContainer } from './Navbar.elements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+
+        </NavbarContainer>
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
